91bab32c14d2161f700c16c3183fdda2b03e8eff
Felipe Bazzarella <fbazzarella@gmail.com>
2014-06-26 18:15:04 -0300

    Refatoração

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   responders :flash
   respond_to :html
 
-  before_filter :authenticate_user!
+  before_action :authenticate_user!
 
   private
     def after_sign_out_path_for(resource_or_scope)

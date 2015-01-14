f93faf5ebe9d78debc3dc89d56f2b281c3591218
Felipe Bazzarella <fbazzarella@gmail.com>
2015-01-13 22:50:51 -0200

    Atualizando lista de Cidades ao alterar Estado com JS

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -18,4 +18,6 @@
 $(function () {
   // $('a[rel~=popover], .has-popover').popover();
   // $('a[rel~=tooltip], .has-tooltip').tooltip();
+
+  $('#realty_country_id').loadCitiesOn('#realty_city_id');
 });

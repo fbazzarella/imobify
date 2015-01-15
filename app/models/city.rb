a0592edb24bcc5ca03fec32b61b89838f71f7125
Felipe Bazzarella <fbazzarella@gmail.com>
2015-01-15 00:37:03 -0200

    Pequena melhoria no código e ajustes de texto

--- app/models/city.rb
+++ app/models/city.rb
@@ -4,5 +4,6 @@ class City < ActiveRecord::Base
   has_many :realties, dependent: :restrict_with_error
   
   validates :country_id, :name, :slug, presence: true
+
   validates :name, :slug, length: {maximum: 255}
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,9 +1,10 @@
 class Country < ActiveRecord::Base
-  has_many :cities, dependent: :restrict_with_error
+  has_many :cities,   dependent: :restrict_with_error
   has_many :realties, dependent: :restrict_with_error
 
   validates :initial, :name, :slug, presence: true
-  validates :initial, length: {is: 2}
+
+  validates :initial,     length: {is: 2}
   validates :name, :slug, length: {maximum: 255}
 
   def self.all_with_cities_by(country)

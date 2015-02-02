bdee6545d4953ae82d4e884f5e6f441123b07af1
Felipe Bazzarella <fbazzarella@gmail.com>
2015-02-02 13:09:30 -0200

    Adicionando escopo para Cidades que tenham Imóveis

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,8 +2,15 @@ class City < ActiveRecord::Base
   belongs_to :country
 
   has_many :realties, dependent: :restrict_with_error
-  
+
   validates :country_id, :name, :slug, presence: true
 
   validates :name, :slug, length: {maximum: 255}
+
+  scope :with_published_realties,
+    -> { distinct.joins(:realties).where(realties: {status: 'published'}).order(:name) }
+
+  def full_name
+    [name, slug.split('-').last.upcase].join(' - ')
+  end
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -12,4 +12,26 @@ RSpec.describe City, type: :model do
   %i(name slug).each do |field|
     it { should ensure_length_of(field).is_at_most(255) }
   end
+
+  describe 'scopes' do
+    describe '#with_published_realties' do
+      let!(:city1) { create(:city) }
+      let!(:city2) { create(:city) }
+
+      let!(:realty1) { create(:realty, city: city1, status: 'published') }
+      let!(:realty2) { create(:realty, city: city1, status: 'published') }
+      let!(:realty3) { create(:realty, city: city2) }
+
+      subject { described_class.with_published_realties }
+
+      it { expect(subject.count).to be_eql(1) }
+      it { expect(subject).to include(city1) }
+    end
+  end
+
+  describe '.full_name' do
+    let!(:city) { create(:city) }
+
+    it { expect(city.full_name).to be_eql('Rio de Janeiro - RJ') }
+  end
 end

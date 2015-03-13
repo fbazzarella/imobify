992086c266d3f7a13a67c714f2d639df4d4afc89
Felipe Bazzarella <fbazzarella@gmail.com>
2015-03-13 19:28:19 -0300

    Validando campo host de Domain

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -3,5 +3,5 @@ class Domain < ActiveRecord::Base
 
   validates :account_id, presence: true
 
-  validates :host, presence: true, uniqueness: true, length: {maximum: 255}
+  validates :host, presence: true, uniqueness: true, length: {maximum: 255}, format: {with: /\A(?:[-a-z0-9]+\.)+[a-z]{2,}\Z/i}
 end
--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -10,4 +10,8 @@ RSpec.describe Domain, type: :model do
   it { should validate_uniqueness_of(:host) }
 
   it { should validate_length_of(:host).is_at_most(255) }
+
+  it { should allow_value('domain.com').for(:host) }
+  it { should_not allow_value('invalid').for(:host) }
+  it { should_not allow_value('domain@invalid').for(:host) }
 end
